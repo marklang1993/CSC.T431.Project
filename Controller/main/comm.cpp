aa2087651d7095d173f532572355cb2f50ae3b8f
Mark Lang <marklang1993@gmail.com>
2018-11-20 14:18:09 +0900

    Refactoring code of initializing tasks.

--- Controller/main/comm.cpp
+++ Controller/main/comm.cpp
@@ -7,6 +7,9 @@
 static const char* ssid = "TP-LINK_D9D564";
 static const char* password = "advanced123";
 
+// Private functions
+static void sendrecv(WiFiClient* pClient);
+
 void commTask(void *pvParameters)
 {
     commInit();
@@ -37,7 +40,33 @@ void commInit(void)
 
 void comm(void)
 {
-    Serial.print("WIFI Idle at: ");
-    Serial.println(millis());
-    delay(5000);
+    static WiFiServer server(80);
+    static WiFiClient client;
+
+    if (client) {
+        // If client is existed
+        if (client.connected()) {
+            // If client is connected
+            Serial.println("Connected to client");
+            sendrecv(&client);
+            delay(500);
+        }
+    }
+    else {
+        // If client does not existed
+        do {
+            // Wait for the client
+            Serial.print("Wait for client");
+            client = server.available();
+            delay(2000);
+        } while(!client);
+    }
+}
+
+static void sendrecv(WiFiClient* pClient)
+{
+    char sendData[256];
+    char recvData[256];
+
+    pClient->write(data);
 }
--- Controller/main/main.ino
+++ Controller/main/main.ino
@@ -4,32 +4,42 @@
 #include "scan.h"
 #include "drive.h"
 
+// Task Descriptor
+typedef struct {
+    TaskFunction_t pvTaskCode;
+    const char *const pcName;
+    const uint32_t usStackDepth;
+    void *const pvParameters;
+    UBaseType_t uxPriority;
+    TaskHandle_t *const pvCreatedTask;
+    const BaseType_t xCoreID;
+} taskDescriptor;
+
+// Task Descriptor Table
+static taskDescriptor tdt[] {
+    {ledTask, "LED", 4096, NULL, 1, NULL, 0}, // LED
+    {commTask, "Communication", 4096, NULL, 2, NULL, 0} // Communication
+};
+
 
 void taskInit() {
     // All tasks should be initialized here
-    xTaskCreatePinnedToCore(
-        ledTask,
-        "LED",
-        4096,
-        NULL,
-        1,
-        NULL,
-        0
-    ); // LED
-
-    xTaskCreatePinnedToCore(
-        commTask,
-        "Communication",
-        4096,
-        NULL,
-        2,
-        NULL,
-        0
-    ); // Communication
+    for (int i = 0; i < sizeof(tdt)/sizeof(taskDescriptor); ++i)
+    {
+        xTaskCreatePinnedToCore(
+            tdt[i].pvTaskCode,
+            tdt[i].pcName,
+            tdt[i].usStackDepth,
+            tdt[i].pvParameters,
+            tdt[i].uxPriority,
+            tdt[i].pvCreatedTask,
+            tdt[i].xCoreID
+        );
+    }
 }
 
 void otherInit() {
-    // Other objects initialization
+    // Other objects should be initialzed here
 
     // Serial Port
     Serial.begin(115200);
