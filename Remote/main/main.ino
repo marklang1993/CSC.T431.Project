4ca93ce11fcbe0219c3ba68f15693c40c86fd106
Cylepsy <cyrus_gao@outlook.com>
2018-12-05 17:01:55 +0900

    Refactoring

--- Remote/main/main.ino
+++ Remote/main/main.ino
@@ -1,39 +1,20 @@
 #include <M5Stack.h>
-#include "./screen1.h"
-#include "./screen2.h"
+#include "./display.h"
 
-bool isScreen1 = true;
 void setup()
 {
-    M5.begin();
-    startRender1();
+    xTaskCreatePinnedToCore(
+        displayTask,
+        "Display",
+        16384,
+        NULL,
+        2,
+        NULL,
+        0
+    );
 }
 
 void loop()
 {
-    if (M5.BtnA.wasPressed())
-    {
-        isScreen1 = !isScreen1;
-        if (isScreen1)
-            startRender1();
-        else
-            startRender2();
-    }
-
-    if (M5.BtnB.wasPressed())
-    {
-        if (isScreen1)
-            ;
-        else
-            next();
-    }
-    if (M5.BtnC.wasPressed())
-    {
-        if (isScreen1)
-            ;
-        else
-            ok();
-    }
-
-    M5.update();
+    vTaskDelete(NULL);
 }
